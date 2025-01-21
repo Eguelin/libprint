9f6b7e1eb03418c8ad967e0a654e8d69e35d374a
Eguelin <eguelin@student.42lyon.fr>
2025-01-21 13:46:04 +0100

    include: Add libprint.h

---
+++ includes/libprint.h
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libprint.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/20 19:44:13 by eguelin           #+#    #+#             */
+/*   Updated: 2025/01/21 13:14:18 by eguelin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBPRINT_H
+# define LIBPRINT_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+# define PRINTF_BUFF_SIZE 1024
+
+typedef enum e_arg_type
+{
+	CHAR,
+	STRING,
+	INT,
+	UINT,
+	HEX,
+	HEX_UP,
+	PTR,
+	PERCENT
+}	t_arg_type;
+
+typedef struct s_arg
+{
+	void		*arg;
+	t_arg_type	type;
+}	t_arg;
+
+typedef struct s_buff
+{
+	char	*buff;
+	size_t	size;
+	size_t	i;
+}	t_buff;
+
+typedef struct s_printf
+{
+	int		fd;
+	int		ret;
+	va_list	ap;
+	t_arg	arg;
+	t_buff	buff;
+}	t_printf;
+
+size_t	ft_strlen(const char *s);
+void	ft_ltobuff(t_buff *buff, t_arg *arg);
+void	ft_ultobuff(t_buff *buff, t_arg *arg, const char *base);
+void	ft_strcpybuff(t_buff *buff, t_arg *arg);
+
+int		ft_printf(const char *format, ...);
+int		ft_dprintf(int fd, const char *format, ...);
+int		ft_sprintf(char *str, const char *format, ...);
+int		ft_snprintf(char *str, size_t size, const char *format, ...);
+
+#endif
